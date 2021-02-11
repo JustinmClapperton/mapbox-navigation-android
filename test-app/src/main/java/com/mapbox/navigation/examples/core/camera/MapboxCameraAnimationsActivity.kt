12d0a4bb068545799b00618a4d625ed520c42464
Raman Lebedinsky <romkaleb@gmail.com>
2021-02-11 13:09:17 +0300

    Removed Timber across SDK. Used logger from MapboxNavigation instead.

--- test-app/src/main/java/com/mapbox/navigation/examples/core/camera/MapboxCameraAnimationsActivity.kt
+++ test-app/src/main/java/com/mapbox/navigation/examples/core/camera/MapboxCameraAnimationsActivity.kt
@@ -16,6 +16,7 @@ import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.base.common.logger.Logger
 import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
@@ -80,6 +81,7 @@ class MapboxCameraAnimationsActivity :
     private lateinit var locationComponent: LocationComponentPlugin
     private lateinit var mapboxMap: MapboxMap
     private lateinit var mapboxNavigation: MapboxNavigation
+    private lateinit var mapboxLogger: Logger
     private val replayRouteMapper = ReplayRouteMapper()
     private val mapboxReplayer = MapboxReplayer()
 
@@ -264,7 +266,7 @@ class MapboxCameraAnimationsActivity :
     }
 
     private fun initRouteLine() {
-        val mapboxRouteLineOptions = MapboxRouteLineOptions.Builder(this)
+        val mapboxRouteLineOptions = MapboxRouteLineOptions.Builder(this, mapboxLogger)
             .withRouteLineBelowLayerId("road-label")
             .build()
         routeLineAPI = MapboxRouteLineApi(mapboxRouteLineOptions)
@@ -366,6 +368,8 @@ class MapboxCameraAnimationsActivity :
             registerRoutesObserver(routesObserver)
             registerMapMatcherResultObserver(mapMatcherResultObserver)
         }
+
+        mapboxLogger = mapboxNavigation.getLogger()
 
         mapboxReplayer.pushRealLocation(this, 0.0)
         mapboxReplayer.playbackSpeed(1.0)
